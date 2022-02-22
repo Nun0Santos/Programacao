b12d9c95b8ed6c5a917156914cdd381fb5dd774e
Nuno Santos <NunoSantos2018@hotmai.com>
2022-02-22 22:38:08 +0000

    Update

--- Guiões/Guião 0/G0Ex1.c
+++ Guiões/Guião 0/G0Ex1.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#define TAM 10
+
+int maiorElemento();
 
 int main(){
+  
+    int tab[TAM] = {2,4,5,9,8,2,4,6,1,3};
+    printf("Maior elemento: %d\n",maiorElemento(tab,TAM));
+
+    return 0;
+}
 
 
-    
+int maiorElemento(int tabela[], int tam){
+    int maior=0;
+    for(int i =0; i<tam; ++i){
+        if(tabela[i] > tabela[i+1]){
+            maior=tabela[i];
+        }
+    }
+    return maior;
+
+
+
 }

583e5efee9223adb155d1a2c18e144e8e6ef8d9f
Nun0Santos <Nunosantos2018@hotmail.com>
2022-06-03 19:07:45 +0100

    Por acabar

--- Guiões/Guião 3/Ex9/main.c
+++ Guiões/Guião 3/Ex9/main.c
@@ -12,8 +12,8 @@ void G3Ex9(char *nameFile){
     FILE *fp;
     int c,nAlunos,nProvas;
     char le[255];
-    int pesosProvas[5],i=0;
-    int a;
+    int pesosProvas[5],i=0,notasProvas[5];
+    int Alunos[5];
 
     fp = fopen(nameFile,"rt");
     if(fp == NULL)
@@ -23,21 +23,27 @@ void G3Ex9(char *nameFile){
         putchar(c);
 
     rewind(fp); // mete o ponteiro fp no inicio do ficheiro
-    
+    putchar('\n');
 
     fscanf(fp,"%*[^:] : %d",&nAlunos);  //%*[^:] le até encontrar : o * permite ler e deitar fora a string
-    printf("\nnAluno: %d\n",nAlunos);
-
     fscanf(fp,"%*[^:] : %d",&nProvas);
-    printf("nProvas: %d\n",nProvas);
 
-    
-    fscanf(fp,"Peso das provas: %d %d %d %d %d",&pesosProvas[i],&pesosProvas[i+1],&pesosProvas[i+2],&pesosProvas[i+3],&pesosProvas[i+4]);
-        
-    for(int i=0; i<5; ++i)
-        printf("Pesos: %d\n",pesosProvas[i]);
+    fscanf(fp,"%*[^:] : %d %d %d %d %d",&pesosProvas[i],&pesosProvas[i+1],&pesosProvas[i+2],&pesosProvas[i+3],&pesosProvas[i+4]);
 
+    for(int i=0; i<nAlunos; ++i){
+        fscanf(fp," %*[^:] : %d %d %d %d %d\n",&notasProvas[i],&notasProvas[i+1],&notasProvas[i+2],&notasProvas[i+3],&notasProvas[i+4]);
+    }
     
+
+    int j= 0;
+    for(int i=0; i<5; ++i){
+        Alunos[j] += (float)(pesosProvas[i]/100) * notasProvas[i];
+        printf("Notas: %d\tPeso: %.2f\n",notasProvas[i],(float)pesosProvas[i]/100);
+        ++j;
+        printf("Percentagem: %d\n",Alunos[j]);
+
+    }
+
     
 
 
